e4a582affd11d4a73c0d250079758653ab5d1810
Javier Dianderas <jdianderas@gmail.com>
2026-06-30 23:37:31 -0500

    Se eliminaron archivos vacios que no corresponden a este entregable

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import { connectDB } from "./config/database.js";
 import express from "express";
 import healthRoutes from "./routes/health.routes.js";
-import usersRouter from "./routes/users.routes.js";
-import sessionsRouter from "./routes/sessions.routes.js";
 import eventsRouter from "./routes/events.routes.js";
-import ticketsRouter from "./routes/tickets.routes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -16,9 +13,6 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/health', healthRoutes);
-app.use('/api/users', usersRouter);
-app.use('/api/sessions', sessionsRouter);
 app.use('/api/events', eventsRouter);
-app.use('/api/tickets', ticketsRouter);
 
 export default app;
