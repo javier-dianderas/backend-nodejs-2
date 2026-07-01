00a071a040f30d277d9780f215de5e0873882f2a
Javier Dianderas <jdianderas@gmail.com>
2026-06-30 23:15:04 -0500

    Se realizaron ajustes por la correccion de teacher. Se descomento el env.example

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URL);
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Base de datos conectada");
     } catch(error) {
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 app.listen(process.env.PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+    console.log(`Servidor escuchando en http://localhost:${process.env.PORT}`);
 });
